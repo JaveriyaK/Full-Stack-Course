d61709d5bad359c3d2fa8e39b95baa35c691fb4d
JaveriyaK <javeriyakumthe004@gmail.com>
2021-11-04 09:47:49 -0700

    null vs undefined

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// string
-console.log(typeof "Hello World")
-console.log(typeof "d")
-
-// Numbers
-console.log(typeof 1)
-console.log(typeof 1.2)
-console.log(typeof 15485641545184841215485148484844862525)
-console.log(typeof -1)
-console.log(typeof -1.2)
-console.log(typeof -15485641545184841215485148484844862525)
-
-// boolean
-
-console.log(typeof true)
-console.log(typeof false)
+var age =null
+// console.log(Name)
+console.log(typeof age)
