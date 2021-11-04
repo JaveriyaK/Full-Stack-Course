26fc29e5cd06a3e2f00a8295fd07174aa306917f
JaveriyaK <javeriyakumthe004@gmail.com>
2021-11-04 07:29:04 -0700

    Primitive Datatype

---
+++ app.js
@@ -0,0 +1,16 @@
+// string
+console.log(typeof "Hello World")
+console.log(typeof "d")
+
+// Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 15485641545184841215485148484844862525)
+console.log(typeof -1)
+console.log(typeof -1.2)
+console.log(typeof -15485641545184841215485148484844862525)
+
+// boolean
+
+console.log(typeof true)
+console.log(typeof false)
