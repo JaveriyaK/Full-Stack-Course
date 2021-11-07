f7a86fea08e850b2ee76508773fead1c720e1d64
JaveriyaK <javeriyakumthe004@gmail.com>
2021-11-07 01:06:24 -0700

    DOM API Selection method

--- app.js
+++ app.js
@@ -1,59 +1,9 @@
-// Operators
+var heading = document.getElementById('heading')
 
-// Arithmetic Operators
+var para = document.getElementsByClassName('para')
 
-var a = 2
-var b = 10
+var tags = document.getElementsByTagName('a')
 
-console.log('a + b=', a + b)
-console.log('b - a=', b - a)
-console.log('b / a=', b / a)
-console.log('b * a=', b * a)
-console.log('b ** a=', b **a)
-console.log('b % a=', b % a)
+var query = document.querySelector('.para')
 
-// Assignment Operators
-
-console.log('b', b)
-b+=10
-console.log('b+=10', b)
-b++
-console.log('b++', b)
-b-=10
-console.log('b-=10', b)
-b--
-console.log('b--', b)
-
-var c = 10
-console.warn(c++)
-console.log(c)
-// console.warc(++c)
-
-// Comparision Operators
-
-var d = 10
-var e = 2
-var f = '10'
-
-console.log(d > e)
-console.log(d < e)
-console.log(d >= e)
-console.log(d <= e)
-console.log(d == e)
-
-console.warn(d == f)
-console.warn(d === f)
-console.log(d + f)
-
-//  Logical Operator
-
-console.log('true && true', true && true)
-console.log('true && false', true && false)
-console.log('false && true', false && true)
-console.log('false && false', false && false)
-
-
-console.log('true || true', true || true)
-console.log('true || false', true || false)
-console.log('false || true', false || true)
-console.log('false ||false', false || false)
+console.log(query)
