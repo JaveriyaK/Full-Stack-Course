28ec1e8f742dfe28494bad2ba208fb59aaccccf0
JaveriyaK <javeriyakumthe004@gmail.com>
2021-11-08 10:48:14 -0800

    dark mode

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
+var body = document.querySelector('body')
 var button = document.querySelector('button')
-var input  = document.querySelector('input')
-var list   = document.querySelector('ul')
+
 
 const  callbackfunc = (event) => 
 {
-    const inputValue = input.value
-    const element = document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
+   body.classList.toggle('dark') 
 }
 
 
