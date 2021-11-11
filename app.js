b988982fcc2f5f464f68c0f6d4cb7a9ea9c752aa
JaveriyaK <javeriyakumthe004@gmail.com>
2021-11-11 10:02:22 -0800

    Array methods

--- app.js
+++ app.js
@@ -1,69 +1,14 @@
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
+var arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
-const add = document.querySelector('#add')
-const multiply = document.querySelector('#multiply')
-const subtract = document.querySelector('#subtract')
-const resultBox = document.querySelector('.result')
+var new_arr = arr.filter((element, index) => 
+{
+    if (element < 100)  return true
+})
 
-const sum = () => {
-   const result = parseInt(a.value) + parseInt(b.value)
-   console.log(result)
-   resultBox.innerHTML = result
-}
-const product = () => {
-   const result = parseInt(a.value) * parseInt(b.value)
-   resultBox.innerHTML = result
-} 
-const sub = () => {
-   const result = parseInt(a.value) - parseInt(b.value)
-   resultBox.innerHTML = result
-}
-
-const calculator = (event ,operation) => {
-//    console.log(event.target)
-//    if (operation == 'add') 
-//    {
-//       console.log('adding')
-//    }
-//    else if (operation == 'multiply')
-//    {
-//       console.log('multiply')
-//    }
+// for(var i = 0; i< arr.length; i++)
+// {
+//    if (arr[i]%2 ==0 ) new_arr.push(arr[i])
 // }
 
-switch (operation) 
-{
-   case "add":
-      resultBox.innerHTML = parseInt(a.value) + 
-      parseInt(b.value)
-      break
-      case "multiply":
-         resultBox.innerHTML =parseInt(a.value) *
-         parseInt(b.value)
-         break
-         case "subtract":
-            resultBox.innerHTML =parseInt(a.value) -
-         parseInt(b.value)
-         break
-         default: 
-         resultBox.innerHTML = "Not a valid operation"
-}
-}
+  console.log(new_arr)
 
-add.addEventListener('click', (event) =>
- {
-    calculator(event, 'add')
-})
-
-
-
-multiply.addEventListener('click', (event) =>
-{
-   calculator(event ,'multiply')
-})
-
-subtract.addEventListener('click', (event) =>
-{
-   calculator(event ,'subtract')
-})
