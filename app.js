c8aff99f85640c11a18b064109801b6f4ddd221b
JaveriyaK <javeriyakumthe004@gmail.com>
2021-11-11 10:25:55 -0800

    splice method

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
-var new_arr = arr.filter((element, index) => 
-{
-    if (element < 100)  return true
-})
+console.log(arr.splice(3, 3))
 
-// for(var i = 0; i< arr.length; i++)
-// {
-//    if (arr[i]%2 ==0 ) new_arr.push(arr[i])
-// }
-
-  console.log(new_arr)
-
+console.log(arr)
