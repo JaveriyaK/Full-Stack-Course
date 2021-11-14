95de9662aa66a0efd5007562833b5f76e0c6ad35
JaveriyaK <javeriyakumthe004@gmail.com>
2021-11-14 12:03:27 -0800

    delete todo items

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => 
-    res.json()).then(data => {
-        console.log(data)
-    })
+  
+  const list = document.querySelector('ul')
+  
+  
+  let arr =  ['One', 'Two', 'Three', 'Four']
+
+  
+  arr.forEach(chore => {
+      const element = document.createElement('li')
+      const textnode = document.createTextNode(chore)
+      element.appendChild(textnode)
+      element.addEventListener('click', (e) => 
+      {
+          const userInput = window.prompt(`Are you sure that
+          you want to delete "${chore}"`)
+          if (userInput === 'YES') e.target.remove()
+      })
+      
+      list.appendChild(element)
+  })
