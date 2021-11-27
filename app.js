ce1f0ee3afd860cd5b6e293b2228217d75f54f4d
JaveriyaK <javeriyakumthe004@gmail.com>
2021-11-27 12:07:12 +0530

    promises hack

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-  
-  const list = document.querySelector('ul')
-  
-  
-  let arr =  ['One', 'Two', 'Three', 'Four']
+const table = document.querySelector('table')
+console.log(table)
 
-  
-  arr.forEach(chore => {
-      const element = document.createElement('li')
-      const textnode = document.createTextNode(chore)
-      element.appendChild(textnode)
-      element.addEventListener('click', (e) => 
-      {
-          const userInput = window.prompt(`Are you sure that
-          you want to delete "${chore}"`)
-          if (userInput === 'YES') e.target.remove()
-      })
-      
-      list.appendChild(element)
-  })
+const CreateRow = (user) => {
+    let newRow = document.createElement('tr')
+    let element = `
+           <td>${user.id}</td>
+           <td>${user.name}</td>
+           <td>${user.email}</td>
+           <td>${user.username}</td>`
+    newRow.innerHTML = element
+    return newRow
+}
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    data.forEach(user => {
+       
+       table.appendChild(CreateRow(user))
+    })
+        
+    
+})
