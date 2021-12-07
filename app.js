7311f8fa00516c168600ab3860d8f4539b34fb5a
JaveriyaK <javeriyakumthe004@gmail.com>
2021-12-07 23:40:30 +0530

    path module

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const path = require('path')
 
-console.log(path.resolve('./app.js'))    //absolute path
+console.log(path.extname('./app.js'))    
 
-console.log(path.relative('/', "./app.js"))    //relative path
+console.log(path.dirname('./utils.js'))
+
+console.log(path.resolve('./app.js'))
+
+console.log(path.relative('./', "./utils.js"))
+
+console.log(path.join(__dirname, 'app.js'))
