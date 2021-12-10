018df28a71db432f805144f6e393cd5e63a048c9
JaveriyaK <javeriyakumthe004@gmail.com>
2021-12-10 17:07:07 +0530

    Creating server without Express sc

--- server.js
+++ server.js
@@ -3,20 +3,42 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((request, response) => {
-    // request.url
-    // console.log(request.headers)
-    var i = path.join(path.resolve(),'index.html')
-    console.log(i)
-    // console.log(path.join(path.resolve(),'index.html'))
-    const markup = fs.readFileSync(i)
-    response.write(markup)
-    response.end()
-})
+    const {url}  = request
+    if(url == '/login')
+    {
+        response.write('<h1>LOGIN</h1>')
+        response.end()
+    }
+
+    if(url == '/signup')
+    {
+        response.write('<h1>SIGNUP</h1>')
+        response.end()
+    }
+    })
+
+    server.listen(3000, () => {
+             console.log(`Server listening at PORT: ${3000}`)
+     })
 
 
-server.listen(3000, () => {
-    console.log(`Server listening at PORT: ${3000}`)
-})
 
-console.log('A message') 
-//  console.log('Another message') 
+
+
+//     // request.url
+//     // console.log(request.headers)
+//     var i = path.join(path.resolve(),'index.html')
+//     console.log(i)
+//     // console.log(path.join(path.resolve(),'index.html'))
+//     const markup = fs.readFileSync(i)
+//     response.write(markup)
+//     response.end()
+// })
+
+
+// server.listen(3000, () => {
+//     console.log(`Server listening at PORT: ${3000}`)
+// })
+
+// console.log('A message')  
+// //  console.log('Another message') 
