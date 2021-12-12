17e3cff6cb68cdd86ac17e7471bed193b02f26de
JaveriyaK <javeriyakumthe004@gmail.com>
2021-12-12 17:48:10 +0530

    extra class

---
+++ database/schema/schema.js
@@ -0,0 +1,19 @@
+//products
+/*
+id : id
+Name : String 
+Price : Number
+Category : category_id
+*/
+
+//Category
+/*
+id : id
+Name : String 
+*/
+
+//order
+/* 
+Total Price : Number
+product : Array of Products [Product_id]
+*/
--- server.js
+++ server.js
@@ -1,23 +1,9 @@
 const express = require('express')
+const database = require('./database/db')
 const app = express()
-const PORT = 3001
 
-app.get('/products/cases')
-app.get('/products/protection')
-app.get('/products/bands')
+app.use(express.json())
 
-app.get('/products/:categories', (req, res) => {
-    if(req.params.categories == 'iphone') {}
-    if(req.params.categories == 'Mac') {}
-    if(req.params.categories == 'ipad') {}
-    if(req.params.categories == 'Music') {}
-    console.log(req.params)
-    res.send("PING")
-})
-
-
-
-
-app.listen(PORT, () => {
-    console.log("Server Listening at port ${PORT}")
+app.listen(3001, () => {
+    console.log("Server Listening at port 3001")
 })
