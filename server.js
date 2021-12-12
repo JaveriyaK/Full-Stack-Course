64f03869ee7099387b3495e30cbd3bcac674b9a4
JaveriyaK <javeriyakumthe004@gmail.com>
2021-12-13 00:26:56 +0530

    d

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const database = require('./database/db')
 const app = express()
+const categoryRoutes = require('./routes/categoryRoutes')
+
+console.log(categoryRoutes.stack)
 
 app.use(express.json())
+//127.0.0.1:3001/mercedes/Routes
+app.use('/category', categoryRoutes)
+
+app.get('/', (req, res) => {
+    try {
+      console.log(document)
+    } catch (error) {
+        res.status(200).send(error.message)
+    }
+
+})
+
 
 app.listen(3001, () => {
     console.log("Server Listening at port 3001")
