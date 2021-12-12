9255a85e3b130d1abd03d3c4776f9f17e7cc9557
JaveriyaK <javeriyakumthe004@gmail.com>
2021-12-13 02:57:51 +0530

    Updated Categories

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,6 +3,11 @@ const express = require('express')
 const router = express.Router()
 const { v4 : uuidv4 } = require('uuid')
 
+/*
+ROUTE: 127.0.0.1:3001/category/all
+METHOD: GET
+*/
+
 router.get('/all', (req, res) => {
     try {
         res.status(200).json({
@@ -19,23 +24,64 @@ router.get('/all', (req, res) => {
     }
 })
 
+/*
+ROUTE: 127.0.0.1:3001/category/add
+METHOD: POST
+*/
+
 router.post('/add', (req, res) => {
+    const {name} = req.body
+    const newCategory = {
+        id : uuidv4(),
+        name
+    }
+    
     try {
-        const { name } = req.body
-
-        let newCategory = {
-            name,
-            id: uuidv4()
-        }
-
-        database.categories.push(newCategory)
-        res.status(200).json({
-            categories: categories,
-            message: error.message,
+        let includes = database.categories.find(item => item.name === name)
+        if(!includes) database.categories.push(newCategory)
+        else console.log('Already exists')
+        res.json({
+            categories: database.categories,
+            message: "Successfully added categories",
             status: "SUCCESS"
         })
     } catch (error) {
-        res.status(200).json({
+        res.json({
+            categories: [],
+            message: error.message,
+            status: "FAILED"
+        })
+    }
+})
+
+
+
+/*
+ROUTE: http://localhost:3001/category/delete
+METHOD: DELETE
+*/
+
+router.delete('/delete/:id', (req, res) => {
+    try {
+
+
+        const { id } = req.params
+        // let element = database.categories.find(item => item.id === id)
+        // const index = database.categories.indexOf(element)
+        // database.categories.splice(index,1)
+
+        const newCategories = database.categories.filter(item => item.id !== id)
+        database.categories = newCategories
+        console.log(newCategories)
+
+        res.json({
+            categories: newCategories,
+            message: "Successfully removed category",
+            status: "SUCCESS"
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
             categories: [],
             message: error.message,
             status: "FAILED"
--- server.js
+++ server.js
@@ -3,20 +3,12 @@ const database = require('./database/db')
 const app = express()
 const categoryRoutes = require('./routes/categoryRoutes')
 
-console.log(categoryRoutes.stack)
 
 app.use(express.json())
-//127.0.0.1:3001/mercedes/Routes
+
 app.use('/category', categoryRoutes)
 
-app.get('/', (req, res) => {
-    try {
-      console.log(document)
-    } catch (error) {
-        res.status(200).send(error.message)
-    }
 
-})
 
 
 app.listen(3001, () => {
