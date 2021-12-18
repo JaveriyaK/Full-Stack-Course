c6017d1751c165615ddf7fe6cb7e84057a5aeb94
JaveriyaK <javeriyakumthe004@gmail.com>
2021-12-19 00:56:44 +0530

    ES-6

---
+++ my-app/src/App.js
@@ -0,0 +1,43 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor,sit amet consectetur
+      adipisicing elit. quaerat possimus, 
+      distinctio asperoius placeat eum 
+      repudiandae ipsa similique nam voluptate
+      neque molestiae blanditilis dignissimos 
+      illum quas expedita ab error accusamtium
+      culpa?
+
+       <h4>
+         I'm a h4
+         </h4>
+
+         <h4>
+         I'm a h4
+         </h4>
+
+         <h4>
+         I'm a h4
+         </h4>
+
+         <h4>
+         I'm a h4
+         </h4>
+
+         <h4>
+         I'm a h4
+         </h4>
+
+         <h4>
+         I'm a h4
+         </h4>
+         
+
+    </div>
+  );
+}
+
+export default App;
